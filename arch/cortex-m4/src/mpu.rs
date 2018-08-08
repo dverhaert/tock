433ef418b97f38b9a2b6a69eeb90d764e09ebcab
Conor McAvity <cmcavity@protonmail.com>
2018-08-07 18:54:48 -0700

    Continue the work boi

--- arch/cortex-m4/src/mpu.rs
+++ arch/cortex-m4/src/mpu.rs
@@ -397,9 +397,9 @@ impl kernel::mpu::MPU for MPU {
             }
         });
 
-        debug!("First update:");
-        debug!("New app memory break: {:#X}", new_app_memory_break as usize);
-        debug!("New new kernel memory break: {:#X}", new_kernel_memory_break as usize);
+        //debug!("First update:");
+        //debug!("New app memory break: {:#X}", new_app_memory_break as usize);
+        //debug!("New new kernel memory break: {:#X}", new_kernel_memory_break as usize);
 
         // The PAM ends at new_app_memory_break, it's different from the region length.
         let pam_end = new_app_memory_break as usize;
@@ -416,8 +416,8 @@ impl kernel::mpu::MPU for MPU {
         let new_subregions_used = pam_size as u32 / region_len * 8 + 1;
 
         if num_subregions_used != new_subregions_used {
-            debug!("Need to update");
             let subregion_mask = (0..new_subregions_used).fold(!0, |res, i| res & !(1 << i)) & 0xff;
+            //let subregion_mask = (0..8).fold(!0, |res, i| res & !(1 << i)) & 0xff;
 
             // Recompute the exponent so we can pass it back into the region config
             // TODO: Move calculation of of region_len_value in Region create function
@@ -620,14 +620,14 @@ impl kernel::mpu::MPU for MPU {
         self.1.map(|config| {
             match config {
                 Some(cortexm_config) => {
-                    debug!("Found cortexmconfig here");
+                    //debug!("Found cortexmconfig here");
                     match cortexm_config.pam_region {
                         Some(pam_region) => {
                             let region_config = pam_region.region;  
-                            debug!("Found region here");
+                            //debug!("Found region here");
                             regs.rbar.write(region_config.base_address);
                             regs.rasr.write(region_config.attributes);
-                            debug!("Wrote PAM registers");
+                            //debug!("Wrote PAM registers");
                         },
                         None => panic!("HUH!?")
 
